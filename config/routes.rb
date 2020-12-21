4adff34e146113e31971b574743b59a55df946fb
swata-dev <74521093+swata-dev@users.noreply.github.com>
2020-12-21 23:09:54 +0900

    routing_revised

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :chefs, controllers: {
-  sessions:      'chefs/sessions',
-  passwords:     'chefs/passwords',
-  registrations: 'chefs/registrations'
-   }
+
   devise_for :users, controllers: {
   sessions:      'users/sessions',
   passwords:     'users/passwords',
   registrations: 'users/registrations'
   }
+
+  devise_for :chefs, controllers: {
+  sessions:      'chefs/sessions',
+  passwords:     'chefs/passwords',
+  registrations: 'chefs/registrations'
+   }
+  
+  devise_scope :chef do
+    get 'profiles', to: 'chefs/registrations#new_profile'
+    post 'profiles', to: 'chefs/registrations#create_profile'
+  end
 
   root to: 'dishes#index'
 
