b97e83233e41fba9f0f3d150ae47115f72ba88f0
swata-dev <74521093+swata-dev@users.noreply.github.com>
2020-12-23 11:52:24 +0900

    dish_model_validated

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,15 @@ class Dish < ApplicationRecord
   belongs_to :chef, optional: true
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :gender_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :ready_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :price
+  end
+
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'should be half-width numbers' }
+  validates :price, numericality: { greater_than: 99, less_than: 10_000_000, message: 'should be within ¥100~¥9,999,999' }
+
 end
