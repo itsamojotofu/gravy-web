737073e1460f4d1da045ef0e504abb5732194b69
swata-dev <74521093+swata-dev@users.noreply.github.com>
2020-12-23 12:42:21 +0900

    dish_price_script

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../dish_price");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
